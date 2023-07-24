689459c1678b89df8672083acd46e385136c622b
G4stLy the Lord <54821580+g4stlyx@users.noreply.github.com>
2023-07-24 13:55:16 +0300

    Add files via upload

--- src/components/navi/Navi.js
+++ src/components/navi/Navi.js
@@ -1,29 +1,30 @@
 import React, { Component } from "react";
 import { Link } from "react-router-dom";
-import { Nav, Navbar, Container } from "react-bootstrap";
+import { Nav, Navbar } from "react-bootstrap";
 import "../../App.css";
 import CartSummary from "../cart/CartSummary";
 
 export default class Navi extends Component {
   render() {
     return (
-      <Navbar expand="lg" className="bg-body-tertiary">
-        <Container>
+      <div className="nav-main">
+      <Navbar expand="lg" className="bg-body-tertiary nav">
+        
           <div class="navBrand">
             <Navbar.Brand href="#"><Link to="/">g4 Store</Link></Navbar.Brand>
           </div>
           <div class="navRight">
-            <Navbar.Toggle aria-controls="basic-navbar-nav" />
-            <Navbar.Collapse id="basic-navbar-nav">
+
+          <Navbar>
               <Nav className="me-auto">
                 <Nav.Link><Link to="/saveproduct">New Product</Link></Nav.Link>
               </Nav>
               &nbsp;&nbsp;
               <CartSummary />
-            </Navbar.Collapse>
+          </Navbar>
           </div>
-        </Container>
       </Navbar>
+      </div>
     );
   }
 }
